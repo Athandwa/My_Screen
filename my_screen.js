bd3b2596ddf62c6aa4e1e391eb6f7956185fb6c7
Lakhiwe <siyasangalakhiwe6@gmail.com>
2017-05-05 10:07:46 +0200

    loaded information and added styles on css

---
+++ my_screen.js
@@ -0,0 +1,22 @@
+function func()
+{
+  var radioBtns = document.getElementsByName("tips")
+
+      if (radioBtns[0].checked) {
+        alert("Lakhiwe");
+      }
+      else if (radioBtns[1].checked) {
+        alert("lucky")
+      }
+
+      else if (radioBtns[2].checked) {
+        alert("luks");
+      }
+
+      else if (radioBtns[3].checked) {
+        alert("lucky2");
+      }
+      else if (radioBtns[4].checked) {
+        alert("lucy");
+      }
+}
